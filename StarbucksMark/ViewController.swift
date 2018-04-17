43edca27e62d77a2d41ee2ec295c025029638161
JerryChou <jerrychou@infinitus-tech.com>
2018-04-17 12:19:02 +0800

    first commit

--- StarbucksMark/ViewController.swift
+++ StarbucksMark/ViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imagePic: UIImageView!
+    @IBOutlet weak var changeRed: UISlider!
+    @IBOutlet weak var changeGreen: UISlider!
+    @IBOutlet weak var changeBlue: UISlider!
+    @IBOutlet weak var labelGreen: UILabel!
+    @IBOutlet weak var labelBlue: UILabel!
+    @IBOutlet weak var labelRed: UILabel!
+    
+    @IBAction func redSliderAct(_ sender: Any) {
+        labelRed.text = "\(CGFloat(changeRed.value))"
+        imagePic.backgroundColor = UIColor(red: CGFloat(changeRed.value), green: CGFloat(changeGreen.value), blue: CGFloat(changeBlue.value), alpha: 1)
+    }
 
+    @IBAction func greenSliderAct(_ sender: Any) {
+        labelGreen.text = "\(CGFloat(changeGreen.value))"
+        imagePic.backgroundColor = UIColor(red: CGFloat(changeRed.value), green: CGFloat(changeGreen.value), blue: CGFloat(changeBlue.value), alpha: 1)
+    }
+    
+    @IBAction func blueSliderAct(_ sender: Any) {
+        labelBlue.text = "\(CGFloat(changeBlue.value))"
+        imagePic.backgroundColor = UIColor(red: CGFloat(changeRed.value), green: CGFloat(changeGreen.value), blue: CGFloat(changeBlue.value), alpha: 1)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
